c27709e9fab1d728474d67b945408e6069d257f6
satya-rakesh <100021003+satya-rakesh@users.noreply.github.com>
2023-02-07 21:39:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// let div = document.createElement('div');
+// // div.classList.add('box');
+
+
+
+// let textNode = document.createTextNode('hello this is newly added section to the web page');
+// div.appendChild(textNode);
+
+// let last = document.querySelector('.containerbox');
+// last.appendChild(div);
+
+
